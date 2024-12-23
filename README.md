6ad17e68eb4e0782bb9a9eff23b981533e959d18
juliano <julianopati92@outlook.com>
2024-12-23 14:29:36 -0300

    adicionando arquivo ao repositório

---
+++ README.md
@@ -0,0 +1,3 @@
+# App-python
+
+Repositório destinado a armazenar o código da aplicação e a imagem de container.
---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def container_id():
+    container_id = socket.gethostname()
+    return "Aplicação rodando no container ID: " + container_id
